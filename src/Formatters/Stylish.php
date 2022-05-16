3bfd5178524e6d2a5a2d63637a7f805cb8366802
Andrey Frolov <internet.buro@gmail.com>
2022-05-17 00:22:36 +0300

    Refactor types array to TYPE_SYMBOLS constant array

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -4,16 +4,16 @@ namespace Differ\Formatters\Stylish;
 
 use function Differ\Formatters\toString;
 
+const TYPE_SYMBOLS = [
+    'deleted' => '-',
+    'added' => '+',
+    'changed' => ' ',
+    'unchanged' => ' '
+];
+
 function stylish(array $diffs): string
 {
-    $types = [
-        'deleted' => '-',
-        'added' => '+',
-        'changed' => ' ',
-        'unchanged' => ' ',
-        'null' => ' '
-    ];
-    $iter = function ($currentDiffs, $depth) use (&$iter, $types) {
+    $iter = function ($currentDiffs, $depth) use (&$iter) {
         if (!is_array($currentDiffs)) {
             return toString($currentDiffs);
         }
@@ -21,7 +21,7 @@ function stylish(array $diffs): string
         $indent = str_repeat("    ", $depth - 1);
 
         $lines = array_map(
-            function ($key, $val) use ($iter, $indent, $depth, $types) {
+            function ($key, $val) use ($iter, $indent, $depth) {
                 if (!is_array($val)) {
                     return "{$indent}    {$key}: " . toString($val);
                 }
@@ -29,10 +29,11 @@ function stylish(array $diffs): string
                     return "{$indent}    {$key}: {$iter($val, $depth + 1)}";
                 }
                 if (!key_exists($val['type'], $val)) {
-                    return "{$indent}  {$types['deleted']} {$key}: {$iter($val['deleted'], $depth + 1)}" . PHP_EOL
-                        . "{$indent}  {$types['added']} {$key}: {$iter($val['added'], $depth + 1)}";
+                    return "{$indent}  " . TYPE_SYMBOLS['deleted'] . " {$key}: {$iter($val['deleted'], $depth + 1)}"
+                        . PHP_EOL
+                        . "{$indent}  " . TYPE_SYMBOLS['added'] . " {$key}: {$iter($val['added'], $depth + 1)}";
                 }
-                return "{$indent}  {$types[$val['type']]} {$key}: {$iter($val[$val['type']], $depth + 1)}";
+                return "{$indent}  " . TYPE_SYMBOLS[$val['type']] . " {$key}: {$iter($val[$val['type']], $depth + 1)}";
             },
             array_keys($currentDiffs),
             $currentDiffs
