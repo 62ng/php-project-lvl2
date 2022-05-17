27cd0bc6d524213f940f4465b4adcd90ef3b1c7b
Andrey Frolov <internet.buro@gmail.com>
2022-05-17 21:54:05 +0300

    Make indent() function in Stylish modelule

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -11,6 +11,11 @@ const TYPE_SYMBOLS = [
     'unchanged' => ' '
 ];
 
+function indent(int $depth, string $sign = ' '): string
+{
+    return str_repeat("    ", $depth) . ($sign === '}' ? '' : "  {$sign} ");
+}
+
 function stylish(array $diffs): string
 {
     $iter = function ($currentDiffs, $depth) use (&$iter) {
@@ -18,29 +23,27 @@ function stylish(array $diffs): string
             return toString($currentDiffs);
         }
 
-        $indent = str_repeat("    ", $depth - 1);
-
         $lines = array_map(
-            function ($key, $val) use ($iter, $indent, $depth) {
+            function ($key, $val) use ($iter, $depth) {
                 if (!is_array($val)) {
-                    return "{$indent}    {$key}: " . toString($val);
+                    return indent($depth) . "{$key}: " . toString($val);
                 }
                 if (!key_exists('type', $val)) {
-                    return "{$indent}    {$key}: {$iter($val, $depth + 1)}";
+                    return indent($depth) . "{$key}: {$iter($val, $depth + 1)}";
                 }
                 if (!key_exists($val['type'], $val)) {
-                    return "{$indent}  " . TYPE_SYMBOLS['deleted'] . " {$key}: {$iter($val['deleted'], $depth + 1)}"
+                    return indent($depth, TYPE_SYMBOLS['deleted']) . "{$key}: {$iter($val['deleted'], $depth + 1)}"
                         . PHP_EOL
-                        . "{$indent}  " . TYPE_SYMBOLS['added'] . " {$key}: {$iter($val['added'], $depth + 1)}";
+                        . indent($depth, TYPE_SYMBOLS['added']) . "{$key}: {$iter($val['added'], $depth + 1)}";
                 }
-                return "{$indent}  " . TYPE_SYMBOLS[$val['type']] . " {$key}: {$iter($val[$val['type']], $depth + 1)}";
+                return indent($depth, TYPE_SYMBOLS[$val['type']]) . "{$key}: {$iter($val[$val['type']], $depth + 1)}";
             },
             array_keys($currentDiffs),
             $currentDiffs
         );
 
-        return implode(PHP_EOL, ['{', ...$lines, "{$indent}}"]);
+        return implode(PHP_EOL, ['{', ...$lines, indent($depth, '}') . '}']);
     };
 
-    return $iter($diffs, 1);
+    return $iter($diffs, 0);
 }
