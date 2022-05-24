0c419fa9742a259b689f74e84e4cee7595b1e5db
Andrey Frolov <internet.buro@gmail.com>
2022-05-24 20:42:21 +0300

    Change indent() function name to makeIndent()

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -11,7 +11,7 @@ const TYPE_SYMBOLS = [
     'unchanged' => ' '
 ];
 
-function indent(int $depth, string $sign = ' '): string
+function makeIndent(int $depth, string $sign = ' '): string
 {
     return str_repeat("    ", $depth) . ($sign === '}' ? '' : "  {$sign} ");
 }
@@ -26,23 +26,23 @@ function formatToStylish(array $diffs): string
         $lines = array_map(
             function ($key, $val) use ($iter, $depth) {
                 if (!is_array($val)) {
-                    return indent($depth) . "{$key}: " . toString($val);
+                    return makeIndent($depth) . "{$key}: " . toString($val);
                 }
                 if (!key_exists('type', $val)) {
-                    return indent($depth) . "{$key}: {$iter($val, $depth + 1)}";
+                    return makeIndent($depth) . "{$key}: {$iter($val, $depth + 1)}";
                 }
                 if (!key_exists($val['type'], $val)) {
-                    return indent($depth, TYPE_SYMBOLS['deleted']) . "{$key}: {$iter($val['deleted'], $depth + 1)}"
+                    return makeIndent($depth, TYPE_SYMBOLS['deleted']) . "{$key}: {$iter($val['deleted'], $depth + 1)}"
                         . PHP_EOL
-                        . indent($depth, TYPE_SYMBOLS['added']) . "{$key}: {$iter($val['added'], $depth + 1)}";
+                        . makeIndent($depth, TYPE_SYMBOLS['added']) . "{$key}: {$iter($val['added'], $depth + 1)}";
                 }
-                return indent($depth, TYPE_SYMBOLS[$val['type']]) . "{$key}: {$iter($val[$val['type']], $depth + 1)}";
+                return makeIndent($depth, TYPE_SYMBOLS[$val['type']]) . "{$key}: {$iter($val[$val['type']], $depth + 1)}";
             },
             array_keys($currentDiffs),
             $currentDiffs
         );
 
-        return implode(PHP_EOL, ['{', ...$lines, indent($depth, '}') . '}']);
+        return implode(PHP_EOL, ['{', ...$lines, makeIndent($depth, '}') . '}']);
     };
 
     return $iter($diffs, 0);
