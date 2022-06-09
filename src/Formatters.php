83d6a63831c41d12ed4f52b40be45cc52556b130
Andrey Frolov <internet.buro@gmail.com>
2022-06-10 00:30:28 +0300

    Refactor toString function

--- src/Formatters.php
+++ src/Formatters.php
@@ -6,18 +6,20 @@ use Differ\Formatters\Stylish;
 use Differ\Formatters\Plain;
 use Differ\Formatters\Json;
 
-function toString(mixed $value, bool $isQuoted = false): string
+function toString(mixed $value, bool $isNeedQuotes = false): string
 {
-    $valueString = trim(var_export($value, true), "' ");
+    $exportedValue = var_export($value, true);
 
+    // var_export почему-то экспортирует null как NULL, а в фикстурах везде null
     if (is_null($value)) {
-        return mb_strtolower($valueString);
+        return mb_strtolower($exportedValue);
     }
     if (is_bool($value) || is_int($value)) {
-        return $valueString;
+        return $exportedValue;
     }
 
-    return $isQuoted ? "'{$valueString}'" : $valueString;
+    $trimedValue = trim($exportedValue, "' ");
+    return $isNeedQuotes ? "'{$trimedValue}'" : $trimedValue;
 }
 
 function format(array $diffs, string $formatter): string
